29d5aeabe480d7b67ecef0b43fbfa30583781d6e
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-13 19:06:48 -0400

    Played with wheel a bit to change default color, wheel size, etc

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var demoColorPicker = new iro.ColorPicker("#color-picker-container");
+var demoColorPicker = new iro.ColorPicker("#color-picker-container", {
+  width: 320,
+  height: 320,
+  color: {r: 163, g: 223, b: 255},
+  borderWidth: 1,
+  borderColor: "#fff"
+});
