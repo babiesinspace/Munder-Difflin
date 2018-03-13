9ccdaee61dd47a6426190f49438a718f43c1b0c4
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-13 19:17:37 -0400

    change color of the swatch-box based on color wheel color

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var demoColorPicker = new iro.ColorPicker("#color-picker-container", {
-  width: 320,
-  height: 320,
+  width: 400,
+  height: 400,
   color: {r: 163, g: 223, b: 255},
   borderWidth: 1,
-  borderColor: "#fff"
+  borderColor: "#fff",
+  css: {
+    "#swatch-box": {
+      "background-color": "$color"
+    }
+  }
 });
