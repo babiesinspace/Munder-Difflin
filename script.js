dff2b2c1fc97e60b7d0059ff622886eb3307c9ca
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-13 19:37:01 -0400

    add event listener to track if a color has been changed, and then show the value of that color as hexidecimal, rgb and hsl

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var demoColorPicker = new iro.ColorPicker("#color-picker-container", {
+let colorBox = new iro.ColorPicker("#color-picker-container", {
   width: 400,
   height: 400,
   color: {r: 163, g: 223, b: 255},
@@ -10,3 +10,18 @@ var demoColorPicker = new iro.ColorPicker("#color-picker-container", {
     }
   }
 });
+
+let colorValues = document.getElementById('color-value')
+
+colorBox.on('color:change', function(color, changes) {
+//  console.log(color.hexString);
+  //check the HSV (opacity scroll?) value too
+  // if (changes.h) {
+  //   console.log(color.hsv);
+  // }
+colorValues.innerHTML = [
+    "hex: " + color.hexString,
+    "rgb: " + color.rgbString,
+    "hsl: " + color.hslString,
+  ].join("<br>");
+})
