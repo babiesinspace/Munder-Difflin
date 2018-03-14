c65f23fb6d08ca0809d628324e1270028b81d27d
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-14 11:57:32 -0400

    forgot to commit. added a color-coded availability display. will check 'stock' of item and color code result. if stock is low, div with similar items will appear. added a lot of css to space out section two (shopping section)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let colorBox = new iro.ColorPicker("#color-picker-container", {
-  width: 400,
+  width: 350,
   height: 400,
   color: {r: 163, g: 223, b: 255},
   borderWidth: 1,
-  borderColor: "#fff",
+  borderColor: "black",
   css: {
     "#swatch-box": {
       "background-color": "$color"
@@ -14,17 +14,51 @@ let colorBox = new iro.ColorPicker("#color-picker-container", {
   }
 });
 
+let randAvailability = ["High - In Stock", "On Order", "Low - Please contact for details"]
+
+//Pick random availability for stock since no data
+function isAvailable() {
+  return randAvailability[Math.floor(Math.random() * randAvailability.length)];
+}
+
+//apply font color based on stock availability (ex: low = red)
+function stoplight(availability){
+  if (availability === randAvailability[0]) {
+    return "green"
+  }
+  if (availability === randAvailability[1]) {
+    return "yellow"
+  }
+  if (availability === randAvailability[2]) {
+    return "red"
+  }
+}
+
+//check if you need to show similar stock items
+function needAlternatives(color) {
+  if (color === "red") {
+    similarStock.classList.remove("hidden")
+  } else {
+    similarStock.classList.add("hidden")
+  }
+}
+
 let colorValues = document.getElementById('color-value')
+let availability = document.getElementById('available')
+let similarStock = document.getElementById('similar')
 
 colorBox.on('color:change', function(color, changes) {
-//  console.log(color.hexString);
-  //check the HSV (opacity scroll?) value too
-  // if (changes.h) {
-  //   console.log(color.hsv);
-  // }
-colorValues.innerHTML = [
+
+  colorValues.innerHTML = [
     "hex: " + color.hexString,
     "rgb: " + color.rgbString,
     "hsl: " + color.hslString,
   ].join("<br>");
+
+  let inStock = isAvailable();
+  availability.innerHTML = inStock;
+  let colorCode = stoplight(inStock);
+  availability.style.color = colorCode;
+  needAlternatives(colorCode);
+
 })
